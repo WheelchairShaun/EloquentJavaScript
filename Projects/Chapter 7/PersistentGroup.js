1fbfa3eb6fd53087ca73bf17b1eead42945a70e1
Shaun Barnes <shaun.w.barnes@gmail.com>
2023-09-12 13:50:51 -0400

    Complete Chapter 7 Exercises

--- Projects/Chapter 7/PersistentGroup.js
+++ Projects/Chapter 7/PersistentGroup.js
@@ -1,46 +1,43 @@
-function runRobot(state, robot, memory) {
-  for (let turn = 0;; turn++) {
-		if (state.parcels.length == 0) {
-			return { turn, memory };
-    }
-    let action = robot(state, memory);
-    state = state.move(action.direction);
-    memory = action.memory;
-  }
-}
-
-function generateTasks()
+class PGroup
 {
-	let output = [];
-	for (let i = 0; i < 100; i++)
+	constructor(members)
 	{
-		output.push(VillageState.random());
+		this.members = members;
 	}
-	return output;
+
+	has(value)
+	{
+		return this.members.includes(value);
+	}
+
+	add(value)
+	{
+		if (!this.has(value))
+		{
+			return new PGroup(this.members.concat([value]));
+		}
+		return this;
+	}
+
+	delete(value)
+	{
+		if (this.has(value))
+		{
+			return new PGroup(this.members.filter(v => v != value));
+		}
+		return this;
+	}
 }
 
-function compareRobots(robot1, memory1, robot2, memory2)
-{
-	let avg1 = 0, avg2 = 0;
-	let history1 = [], history2 = [];
-	let tasks = generateTasks();
+PGroup.empty = new PGroup([]);
 
-	for (let task of tasks)
-	{
-		history1.push(runRobot(task, robot1, memory1));
-		history2.push(runRobot(task, robot2, memory2));
-	}
+let a = PGroup.empty.add("a");
+let ab = a.add("b");
+let b = ab.delete("a");
 
-	for (let h = 0; h < 100; h++)
-	{
-		avg1 += history1[h].turn;
-		avg2 += history2[h].turn;
-	}
-
-	avg1 = avg1 / 100;
-	avg2 = avg2 / 100;
-
-	console.log(`Robot1 Average Steps: ${avg1}, Robot2 Average Steps: ${avg2}`);
-}
-
-compareRobots(routeRobot, [], goalOrientedRobot, []);
+console.log(b.has("b"));
+// → true
+console.log(a.has("b"));
+// → false
+console.log(b.has("a"));
+// → false
